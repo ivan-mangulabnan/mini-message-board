9d0d0d7d7257d0062b001a5fee030b7963208524
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-10-27 15:25:42 +0800

    add bases and routes

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import newRouter from './routes/new';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = 6969;
+
+const messages = [];
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/new', newRouter);
+
+app.get('/', (req, res) => {
+  res.render('index', { messages });
+})
 
 app.listen(PORT, (err) => {
   if (err) {
