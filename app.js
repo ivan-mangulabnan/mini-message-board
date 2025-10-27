e785581cf2050490c159589388bb6eeb5053897d
Ivan Mangulabnan <ivanmangulabnan19@gmail.com>
2025-10-27 16:15:40 +0800

    added a navbar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import newRouter from './routes/new';
+import newRouter from './routes/new.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,6 +9,10 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = 6969;
 
+const links = [
+  { href: '/', name: 'Home' },
+  { href: '/new', name: 'New Message' }
+]
 const messages = [];
 
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +21,7 @@ app.set('view engine', 'ejs');
 app.use('/new', newRouter);
 
 app.get('/', (req, res) => {
-  res.render('index', { messages });
+  res.render('index', { links, messages });
 })
 
 app.listen(PORT, (err) => {
